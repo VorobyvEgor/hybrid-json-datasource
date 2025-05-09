50c2bc358ce3b1b7e922bf28e043231705ae23bd
egorvorobev <vorobyv.egor@gmail.com>
2025-05-09 22:46:03 +0300

    Create toJsonString()

---
+++ src/main/scala/org/apache/spark/sql/hybrid/RowConverter.scala
@@ -0,0 +1,37 @@
+package org.apache.spark.sql.hybrid
+
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.json.{JSONOptions, JacksonGenerator}
+import org.apache.spark.sql.catalyst.util.{ArrayData, MapData}
+import org.apache.spark.sql.types.{ArrayType, MapType, StructType}
+import org.apache.spark.unsafe.types.UTF8String
+
+import java.io.CharArrayWriter
+
+class RowConverter(dataType: StructType) {
+  def toJsonString(input: Iterator[InternalRow]): Iterator[String] = {
+
+    lazy val writer = new CharArrayWriter()
+
+    lazy val gen = new JacksonGenerator(
+      dataType, writer, new JSONOptions(Map.empty[String, String], defaultTimeZoneId = "UTC"))
+
+    lazy val converter: Any => UTF8String = {
+      def getAndReset(): UTF8String = {
+        gen.flush()
+        val json = writer.toString
+        writer.reset()
+        UTF8String.fromString(json)
+      }
+
+      dataType match {
+        case _: StructType =>
+          (row: Any) =>
+            gen.write(row.asInstanceOf[InternalRow])
+            getAndReset()
+      }
+    }
+
+    Iterator(converter(input.next()).toString)
+  }
+}
